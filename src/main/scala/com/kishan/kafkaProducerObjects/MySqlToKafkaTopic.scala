ff7f7490d5ac62b7cf782d85c3c3d208e1ca9674
Kishan <suriya.kishan@live.com>
2020-05-03 21:56:26 +0530

    Added MySql to Kafka Producer Application

---
+++ src/main/scala/com/kishan/kafkaProducerObjects/MySqlToKafkaTopic.scala
@@ -0,0 +1,43 @@
+package com.kishan.kafkaProducerObjects
+
+import com.kishan.mysql.MySqlToKafkaProducer.sendMessageAsJson
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.common.serialization.StringSerializer
+
+import scala.collection.JavaConverters._
+
+/*
+* This singleton scala object will make use of method defined in the com.kishan.mysql.MySqlToKafkaProducer and
+* send message into kafka topic as JSON
+* */
+
+object MySqlToKafkaTopic {
+  def main(args: Array[String]): Unit = {
+    val inputFilePath = "C:\\Users\\Kishan\\IdeaProjects\\kafkaToMySql\\sampleFiles\\sample_messages_50.csv"
+
+    val topicName = "TestTopic"
+    val bootStrapServers = "192.168.181.128:9092,192.168.181.128:9093,192.168.181.128:9094"
+
+    val producerProperties: java.util.Map[String, Object] = Map[String, Object](
+      "bootstrap.servers" -> bootStrapServers,
+      "key.serializer" -> classOf[StringSerializer],
+      "value.serializer" -> classOf[StringSerializer]
+    ).asJava
+
+    val kafkaProducer: KafkaProducer[String, String] = new KafkaProducer[String, String](producerProperties)
+    val keysList: List[String] = "id,countrycode".split(",").map(a => a.trim).toList
+    val sqlQuery = "select * from world.city where lower(countrycode)='ind'"
+    println(s"Producing message to topic: ${topicName}")
+    try {
+      sendMessageAsJson(kafkaProducer, topicName, sqlQuery, keysList)
+      println("All message sent to topic successfully.")
+    } catch {
+      case e: Exception => {
+        println(s"Caught exception: ${e.getMessage}")
+        e.printStackTrace()
+      }
+    } finally {
+      kafkaProducer.close()
+    }
+  }
+}
---
+++ src/main/scala/com/kishan/mysql/MySqlToKafkaProducer.scala
@@ -0,0 +1,61 @@
+package com.kishan.mysql
+
+import java.sql.{Connection, DriverManager}
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.json.JSONObject
+
+import scala.collection.JavaConverters._
+import scala.io.Source
+
+/*
+* This scala object consist of method will get the data from the MySql table and create JSON for each record.
+* The JSON will be send to kafka topic.
+* The key for message will be concatenated value of the list of keys provided as input.
+* */
+
+
+object MySqlToKafkaProducer {
+
+  def sendMessageAsJson(producer: KafkaProducer[String, String], topicName: String, sqlQuery: String, recordKeys: List[String]): Unit = {
+    //Reading Properties File
+    val propertiesFile = Source.fromResource("mysql.properties").getLines().map(a => a.split("=")).map(a => (a(0), a(1))).toMap.asJava
+
+    val dbProperties = new Properties()
+    dbProperties.putAll(propertiesFile)
+
+    //Create JDBC Connection
+    val connectionURL: String = dbProperties.get("com.kishan.db.mysql.jdbc_url").toString
+    val userName: String = dbProperties.get("com.kishan.db.mysql.username").toString
+    val password: String = dbProperties.get("com.kishan.db.mysql.password").toString
+
+    val conn: Connection = DriverManager.getConnection(connectionURL, userName, password)
+
+    val createStatement = conn.createStatement()
+    val resultset = createStatement.executeQuery(sqlQuery)
+    val resultMetadata = resultset.getMetaData
+    var columns: List[String] = List()
+    println("Table Information")
+    for (i <- 1 to resultMetadata.getColumnCount) {
+      println(s"Column Index: ${i}, Column Name: ${resultMetadata.getColumnName(i)}, Column Type: ${resultMetadata.getColumnTypeName(i)}")
+      columns = columns :+ resultMetadata.getColumnName(i)
+    }
+
+    // Creating JSON and Sending record in to kafka topic
+    while (resultset.next()) {
+      var json: JSONObject = new JSONObject()
+      var key: String = ""
+      for (column <- columns) {
+        val columnValue = resultset.getObject(column)
+        if (recordKeys.contains(column.toLowerCase)) {
+          key = key + columnValue.toString
+        }
+        json.put(column, columnValue)
+      }
+      val producerRecord: ProducerRecord[String, String] = new ProducerRecord[String, String](topicName, key, json.toString())
+      producer.send(producerRecord)
+    }
+    conn.close()
+  }
+}
